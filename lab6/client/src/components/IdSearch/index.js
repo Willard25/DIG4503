65cba3a7d5bc88f6f963a9164d5522222454eb51
Willard25 <47044174+Willard25@users.noreply.github.com>
2020-02-22 19:51:21 -0500

    lab6

---
+++ lab6/client/src/components/IdSearch/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class IdSearch extends React.Component {
+
+    readId(event) {
+        event.preventDefault();
+        let element = document.querySelector("#ID");
+        fetch("/id/" + element.value)
+        .then((res) => {
+            return res.json();
+        })
+        .then((processed) => {
+            let reporting = document.querySelector("#reportingArea");
+
+            if(processed.error) {
+                reporting.innerHTML = processed.error;
+            } else {
+                reporting.innerHTML = processed.name;
+            }
+        });
+
+        element.value = "";
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Id</h2>
+                <form onSubmit={this.readId}>
+                    <input id="ID" type="text" />
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+
+}
+
+export default IdSearch;
---
+++ lab6/index.js
@@ -0,0 +1,46 @@
+const Express = require("express")
+const App = Express();
+const port = 80;
+
+const chalk = require('chalk');
+const getPokemons = require('json-pokemon/getPokemon');
+
+let pokemons = getPokemons();
+
+const cors = require("cors");
+App.use(cors());
+
+App.use("/", Express.static("client/build"));
+
+App.get("/id/:id", (req, res) => {
+
+    let result = {"error": "Not found!"};
+
+    pokemons.forEach((value) => {
+        if(req.params.id == value.id) {
+            result = value;
+            console.log(chalk.green(__filename));
+        }
+        console.log(chalk.red(__filename));
+    });
+
+    res.json(result);
+});
+
+App.get("/name/:name", (req, res) => {
+    let result = {"error": "Not found!"};
+
+    pokemons.forEach((value) => {
+        if(req.params.name == value.name) {
+            result = value;
+            console.log(chalk.green(__filename));
+        }
+        console.log(chalk.red(__filename));
+    });
+
+    res.json(result);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");   
+});
