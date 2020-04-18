0450bdbbc7dba3c7f26fcf14a53a33e0f52bbbd8
Willard25 <47044174+Willard25@users.noreply.github.com>
2020-04-18 19:57:59 -0400

    docs upload

---
+++ docs/precache-manifest.5c59c753ab21866652cb9f25750b3180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe483fff402643e68adc5e615b060208",
+    "url": "/DIG4503/index.html"
+  },
+  {
+    "revision": "65ff2daec6736d9ef7ee",
+    "url": "/DIG4503/static/css/main.c1ac1655.chunk.css"
+  },
+  {
+    "revision": "deb4ed883d1cc24d80c7",
+    "url": "/DIG4503/static/js/2.af4cae00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DIG4503/static/js/2.af4cae00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ff2daec6736d9ef7ee",
+    "url": "/DIG4503/static/js/main.17d64824.chunk.js"
+  },
+  {
+    "revision": "7bc97310f8435691c79a",
+    "url": "/DIG4503/static/js/runtime-main.ea8dba9c.js"
+  }
+]);
